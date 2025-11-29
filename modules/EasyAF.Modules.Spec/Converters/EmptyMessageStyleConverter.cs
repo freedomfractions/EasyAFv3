d341320d9e1f23b1b874b44852c014df598a6e45
freedomfractions <brandon.buscher.ee@gmail.com>
2025-11-29 08:18:21 -0600

    Add live formatting preview to Empty Message TextBox + swap alignment order (vertical first) + change vertical 'Center' to 'Middle'

---
+++ modules/EasyAF.Modules.Spec/Converters/EmptyMessageStyleConverter.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EasyAF.Modules.Spec.Converters
+{
+    /// <summary>
+    /// Multi-value converter that converts empty message formatting properties to WPF styling attributes.
+    /// Used to provide live preview of formatting in the empty message TextBox.
+    /// </summary>
+    public class EmptyMessageFontFamilyConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string fontName && !string.IsNullOrWhiteSpace(fontName))
+            {
+                try
+                {
+                    return new FontFamily(fontName);
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageFontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string sizeStr && double.TryParse(sizeStr, out var size) && size > 0)
+            {
+                return size;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool bold && bold)
+            {
+                return FontWeights.Bold;
+            }
+            return FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageFontStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool italic && italic)
+            {
+                return FontStyles.Italic;
+            }
+            return FontStyles.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageTextDecorationConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool underline && underline)
+            {
+                return TextDecorations.Underline;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageBackgroundConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string hex && !string.IsNullOrWhiteSpace(hex))
+            {
+                try
+                {
+                    var color = (Color)ColorConverter.ConvertFromString("#" + hex.TrimStart('#'));
+                    return new SolidColorBrush(color);
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageForegroundConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string hex && !string.IsNullOrWhiteSpace(hex))
+            {
+                try
+                {
+                    var color = (Color)ColorConverter.ConvertFromString("#" + hex.TrimStart('#'));
+                    return new SolidColorBrush(color);
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageHorizontalAlignmentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string align && !string.IsNullOrWhiteSpace(align))
+            {
+                return align.ToLowerInvariant() switch
+                {
+                    "left" => TextAlignment.Left,
+                    "center" => TextAlignment.Center,
+                    "right" => TextAlignment.Right,
+                    _ => TextAlignment.Left
+                };
+            }
+            return TextAlignment.Left;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmptyMessageVerticalAlignmentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string align && !string.IsNullOrWhiteSpace(align))
+            {
+                return align.ToLowerInvariant() switch
+                {
+                    "top" => VerticalAlignment.Top,
+                    "center" => VerticalAlignment.Center,
+                    "middle" => VerticalAlignment.Center,
+                    "bottom" => VerticalAlignment.Bottom,
+                    _ => VerticalAlignment.Center
+                };
+            }
+            return VerticalAlignment.Center;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
