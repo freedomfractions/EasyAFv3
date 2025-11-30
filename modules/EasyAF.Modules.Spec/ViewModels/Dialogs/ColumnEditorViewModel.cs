cc631570af660092d26df76aef357207eaf220d0
freedomfractions <brandon.buscher.ee@gmail.com>
2025-11-29 20:43:01 -0600

    Phase 2: Add Basic Settings Panel to Column Editor (Header, Width%, MergeVertically)

--- modules/EasyAF.Modules.Spec/ViewModels/Dialogs/ColumnEditorViewModel.cs
+++ modules/EasyAF.Modules.Spec/ViewModels/Dialogs/ColumnEditorViewModel.cs
@@ -28,6 +28,10 @@ namespace EasyAF.Modules.Spec.ViewModels.Dialogs
         private readonly IPropertyDiscoveryService _propertyDiscovery;
         private readonly ISettingsService _settingsService;
 
+        private string _columnHeader = string.Empty;
+        private double? _widthPercent;
+        private bool _mergeVertically;
+
         /// <summary>
         /// Initializes a new instance of the ColumnEditorViewModel.
         /// </summary>
@@ -42,6 +46,9 @@ namespace EasyAF.Modules.Spec.ViewModels.Dialogs
             _propertyDiscovery = propertyDiscovery ?? throw new ArgumentNullException(nameof(propertyDiscovery));
             _settingsService = settingsService ?? throw new ArgumentNullException(nameof(settingsService));
 
+            // Load current values from ColumnSpec
+            LoadFromColumnSpec();
+
             // Initialize commands
             OkCommand = new DelegateCommand(ExecuteOk, CanExecuteOk);
             CancelCommand = new DelegateCommand(ExecuteCancel);
@@ -51,6 +58,24 @@ namespace EasyAF.Modules.Spec.ViewModels.Dialogs
         }
 
         #region Properties
+
+        public string ColumnHeader
+        {
+            get => _columnHeader;
+            set => SetProperty(ref _columnHeader, value);
+        }
+
+        public double? WidthPercent
+        {
+            get => _widthPercent;
+            set => SetProperty(ref _widthPercent, value);
+        }
+
+        public bool MergeVertically
+        {
+            get => _mergeVertically;
+            set => SetProperty(ref _mergeVertically, value);
+        }
 
         public bool? DialogResult { get; private set; }
 
@@ -67,14 +92,21 @@ namespace EasyAF.Modules.Spec.ViewModels.Dialogs
 
         private bool CanExecuteOk()
         {
-            // For now, always allow OK
-            // Will add validation as we build panels
+            // Must have a column header
+            if (string.IsNullOrWhiteSpace(ColumnHeader))
+                return false;
+
             return true;
         }
 
         private void ExecuteOk()
         {
-            Log.Information("Column editor OK clicked (no changes yet - panels not implemented)");
+            // Update the ColumnSpec with current values
+            _columnSpec.Header = ColumnHeader;
+            _columnSpec.WidthPercent = WidthPercent;
+            _columnSpec.MergeVertically = MergeVertically;
+
+            Log.Information("Column updated: {Header}", ColumnHeader);
 
             DialogResult = true;
             CloseDialog();
@@ -96,6 +128,19 @@ namespace EasyAF.Modules.Spec.ViewModels.Dialogs
             {
                 window.DialogResult = DialogResult;
             }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void LoadFromColumnSpec()
+        {
+            _columnHeader = _columnSpec.Header ?? string.Empty;
+            _widthPercent = _columnSpec.WidthPercent;
+            _mergeVertically = _columnSpec.MergeVertically;
+
+            RaisePropertyChanged(string.Empty); // Refresh all bindings
         }
 
         #endregion
---
+++ modules/EasyAF.Modules.Spec/Views/Dialogs/Panels/ColumnBasicSettingsPanel.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace EasyAF.Modules.Spec.Views.Dialogs.Panels
+{
+    /// <summary>
+    /// Basic settings panel for column editor (Header, Width, MergeVertically).
+    /// </summary>
+    public partial class ColumnBasicSettingsPanel : UserControl
+    {
+        public ColumnBasicSettingsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
